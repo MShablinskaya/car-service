caed8ac36342e5164c2f1f2833f3bc8cced37749
M.Shablinskaya <marie.macheri@gmail.com>
2022-08-24 15:57:57 +0300

    fixed entities+dto

---
+++ src/main/java/com/innowise/sharing/dto/UserDto.java
@@ -0,0 +1,14 @@
+package com.innowise.sharing.dto;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class UserDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private DocumentDto licenceId;
+}
