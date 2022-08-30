ab4617dacae807d2938eeb0204ae23e52c22efa9
YanaV267 <yanochka.volkova.2002@gmail.com>
2022-08-30 17:18:41 +0300

    specified car schema

--- src/main/java/com/innowise/sharing/kafka/producer/impl/CarMessageProducerImpl.java
+++ src/main/java/com/innowise/sharing/kafka/producer/impl/CarMessageProducerImpl.java
@@ -1,14 +1,14 @@
 package com.innowise.sharing.kafka.producer.impl;
 
 import com.innowise.sharing.entity.Car;
+import com.innowise.sharing.kafka.mapper.RecordMapper;
 import com.innowise.sharing.kafka.producer.MessageProducer;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.generic.GenericRecordBuilder;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -20,37 +20,20 @@ import java.io.File;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class CarMessageProducerImpl implements MessageProducer<Car> {
     private static final String FILE_PATH =
             "G:\\Work\\Innowise\\AutoSharing\\car-service\\src\\main\\resources\\kafka.avro\\car-schema.avsc";
     private final KafkaTemplate<String, GenericRecord> kafkaTemplate;
+    private final RecordMapper<Car> carRecordMapper;
     @Value(value = "${spring.kafka.topic.name}")
     private String topicName;
-
-    @Autowired
-    public CarMessageProducerImpl(KafkaTemplate<String, GenericRecord> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
 
     @SneakyThrows
     @Override
     public void send(Car car) {
         Schema schema = new Schema.Parser().parse(new File(FILE_PATH));
-        GenericRecord ownerRecord = new GenericRecordBuilder(schema.getField("owner").schema())
-                .set("first_name", car.getOwnerId().getFirstName())
-                .set("last_name", car.getOwnerId().getLastName())
-                .set("email", car.getOwnerId().getEmail())
-                .build();
-        GenericRecord carRecord = new GenericRecordBuilder(schema)
-                .set("id", car.getId())
-                .set("brand", car.getBrand())
-                .set("model", car.getModel())
-                .set("color", car.getColor())
-                .set("release_year", car.getReleaseYear().getYear())
-                .set("registration_number", car.getRegistrationNumber())
-                .set("availability", car.getAvailability())
-                .set("owner", ownerRecord)
-                .build();
+        GenericRecord carRecord = carRecordMapper.mapToRecord(car, schema);
         ProducerRecord<String, GenericRecord> producerRecord = new ProducerRecord<>(topicName, carRecord);
         ListenableFuture<SendResult<String, GenericRecord>> future = kafkaTemplate.send(producerRecord);
         future.addCallback(new ListenableFutureCallback<>() {
